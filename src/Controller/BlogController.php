5ae3fec5eba3d945f8b677c03b02e9ac3140d4e3
Andrew672 <amarbach5@gmail.com>
2022-03-02 13:00:32 +0100

    Ajout du Form Controller

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -9,9 +9,18 @@ use App\Entity\Article;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class BlogController extends AbstractController
 {
+
+protected $em;
+
+public function __construct(EntityManagerInterface $entityManager)
+{
+    $this->em = $entityManager;
+}
     /**
      * @Route("/blog", name="app_blog")
      */
@@ -35,18 +44,36 @@ class BlogController extends AbstractController
     }
     /**
     * @Route("/blog/new", name="blog_create")
+    * @Route("/blog/{id}/edit", name="blog_edit")
     */
-    public function create(){
-        $article = new Article();
+    public function form(Article $article = null,Request $request, EntityManagerInterface $manager){
+
+        if(!$article){
+            $article = new Article();
+        }
+        //$article = new Article();
 
         $form = $this->createFormBuilder($article)
                      ->add('title')
                      ->add('content')
                      ->add('image')
-                     
                      ->getForm();
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            if(!$article->getId()){
+                $article->setCreatedAt(new \DateTimeImmutable());
+            }
+            
+            $em = $this->em;
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show',['id' => $article->getId()]);
+        }
         return $this->render('/blog/create.html.twig',[
-            'formArticle' => $form->createView()
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !== null
         ]);
     }
 
@@ -54,11 +81,18 @@ class BlogController extends AbstractController
      * @Route("/blog/{id}", name="blog_show")
      */
     public function show($id){
+        
         $repo = $this->getDoctrine()->getRepository(Article::class);
         $article = $repo->find($id);
+        if(! $article)
+        {
+            return $this->render('blog/error404.html.twig');
+        }
+        else{
         return $this->render('blog/show.html.twig',[
             'article' => $article
         ]);
+    }
     }
     
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\Url as U;
+use Symfony\Component\Validator\Constraints\Length as L;
 
 /**
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
@@ -19,16 +21,27 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @L(
+     *      min = 10,
+     *      max = 50,
+     *      minMessage = "Your first name must be at least {{ limit }} characters long",
+     *      maxMessage = "Your first name cannot be longer than {{ limit }} characters"
+     * )
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @L(min=10)
      */
     private $content;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @U(
+     *    protocols = {"http", "https", "ftp"},
+     *    message = "The url '{{ value }}' is not a valid url",
+     * )
      */
     private $image;
 
